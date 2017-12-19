9c1231d3abe6047037ad9bb55d7a9c09491e0500
tsurubee <29603512+tsurubee@users.noreply.github.com>
2017-12-19 21:51:15 +0900

    Add pipe_reverse.py

--- unix_processes/pipe_reverse.py
+++ unix_processes/pipe_reverse.py
@@ -1,19 +1,19 @@
 #パイプを用いたプロセス間通信
-#親→子の通信
+#子→親の通信
 import os
 
 reader, writer = os.pipe()
 
 if os.fork():
     # Parent process
-    os.close(reader)
-    write_pipe = os.fdopen(writer, 'w')
-    write_pipe.write('Hello child!')
-    write_pipe.close()
-else:
-    # Child process
     os.close(writer)
     read_pipe = os.fdopen(reader, 'r')
     message = read_pipe.readline()
     read_pipe.close()
     print(message)
+else:
+    # Child process
+    os.close(reader)
+    write_pipe = os.fdopen(writer, 'w')
+    write_pipe.write('Hello parent!')
+    write_pipe.close()
