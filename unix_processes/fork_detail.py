db6312e514be7aa9c02bc28b05de81ba567654ed
tsurubee <29603512+tsurubee@users.noreply.github.com>
2017-12-24 15:04:01 +0900

    Change file name of fork2.py

--- unix_processes/fork_detail.py
+++ unix_processes/fork_detail.py
@@ -1,15 +1,14 @@
 import os
 
-print("parent process pid: {0}".format(os.getpid()))
+print("---- before fork ----")
+print("pid: {0}".format(os.getpid()))
 ret = os.fork()
 
 if ret:
     print("----- if block ------")
     print("pid: {0}".format(os.getpid()))
     print("return value: {0}".format(ret))
-    print("---------------------")
 else:
     print("---- else block -----")
     print("pid: {0}".format(os.getpid()))
     print("return value: {0}".format(ret))
-    print("---------------------")
